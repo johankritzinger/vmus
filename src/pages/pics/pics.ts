af0171fcbe43902a57c8b5df2a7bb4c00c97b669
Johan Kritzinger <johan.kritzinger@woodmac.com>
2017-12-29 11:17:03 +0000

    some tidying up

--- src/pages/pics/pics.ts
+++ src/pages/pics/pics.ts
@@ -46,46 +46,28 @@ export class PicsPage {
     this.form = formBuilder.group(this.vmrecords.record);
   }
 
-  // public presentActionSheet(picform) {
-  //   let actionSheet = this.actionSheetCtrl.create({
-  //     title: 'Select Image Source',
-  //     buttons: [
-  //       {
-  //         text: 'Load from Library',
-  //         handler: () => {
-  //           this.takePicture(this.camera.PictureSourceType.PHOTOLIBRARY,picform);
-  //         }
-  //       },
-  //       {
-  //         text: 'Use Camera',
-  //         handler: () => {
-  //           this.takePicture(this.camera.PictureSourceType.CAMERA,picform);
-  //         }
-  //       },
-  //       {
-  //         text: 'Cancel',
-  //         role: 'cancel'
-  //       }
-  //     ]
-  //   });
-  //   actionSheet.present();
-  // }
-
-// This is wrong, replace with below
-  public presentActionSheet(picform) {
+  public getPicture(picform,source) {
     var sourceType = this.camera.PictureSourceType.CAMERA;
-// Replace up to here
-
-    // public takePicture(sourceType,picform) {
     // Create options for the Camera Dialog
     var options = {
       quality: 100,
       sourceType: sourceType,
       saveToPhotoAlbum: true,
       correctOrientation: true,
-      allowEdit: true
+      allowEdit: false
     }
-
+    if (source == 'CAMERA') {
+      // default settings correct
+    } else if (source == 'PHOTOLIBRARY') {
+      sourceType = this.camera.PictureSourceType.PHOTOLIBRARY;
+      options = {
+        quality: 100,
+        sourceType: sourceType,
+        saveToPhotoAlbum: true,
+        correctOrientation: true,
+        allowEdit: true
+      }
+    }
     // Get the data of an image
      this.camera.getPicture(options).then((imagePath) => {
        // Special handling for Android library
@@ -139,7 +121,7 @@ export class PicsPage {
     if (img === null) {
       return '';
     } else {
-      this.presentToast(cordova.file.dataDirectory + img);
+      // this.presentToast(cordova.file.dataDirectory + img);
       return cordova.file.dataDirectory + img;
     }
   }
